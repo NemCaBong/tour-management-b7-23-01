a8514a1834e7557a5386361a91f3ceb6f029dce7
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-18 23:00:26 +0700

    1.5. Lấy ra danh sách Tour

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database";
+import Tour from "./models/tour.model";
 
 dotenv.config();
 
@@ -12,8 +13,22 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", (req: Request, res: Response) => {
-  res.render("client/pages/tours/index");
+app.get("/tours", async (req: Request, res: Response) => {
+  // SELECT * FROM tours WHERE deleted = false AND status = "active";
+  const tours = await Tour.findAll({
+    where: {
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  });
+
+  console.log(tours);
+
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
+    tours: tours
+  });
 });
 
 app.listen(port, () => {
